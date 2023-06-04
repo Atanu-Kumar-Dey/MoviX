6bcef7e270b861c34461c610e0fb714ca6463172
Atanu Kumar Dey <atanukumardey_2020@gift.edu.in>
2023-06-04 13:28:20 +0530

    banner photo glitch resolved

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -15,7 +15,7 @@ const HeroBanner = () => {
   const { data, loading } = useFetch(`/movie/upcoming`);
   useEffect(() => {
     const bg =
-      url.backdrop +
+      "https://image.tmdb.org/t/p/original" +
       data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
     setBackground(bg);
   }, [data]);
